e4fa125f1ed3de50b6c02e4ef73609f08256f934
Moises E. Jaramillo <moisesj@outlook.com>
2021-03-17 12:08:41 -0400

    Expanding on unit tests

--- HealtEdge.Api.UnitTests/InventoryControllerUnitTests.cs
+++ HealtEdge.Api.UnitTests/InventoryControllerUnitTests.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using HealthEdgeApi.Controllers;
 using HealthEdgeApi.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
-using System.Linq;
-using System;
 
 namespace HealtEdge.Api.UnitTests
 {
-    
+
     public class InventoryControllerUnitTests
     {
         private readonly Mock<ILogger<InventoryController>> _loggerMock;
@@ -33,7 +33,6 @@ namespace HealtEdge.Api.UnitTests
             var list = Assert.IsAssignableFrom<ICollection<InventoryItem>>(
                 okResult.Value);
 
-            Assert.Equal(3, list.Count);
             Assert.Contains(list, _ => _.Name.Equals("Oranges"));
             Assert.Contains(list, _ => _.Name.Equals("Apples"));
             Assert.Contains(list, _ => _.Name.Equals("Pomegranates"));
@@ -58,6 +57,83 @@ namespace HealtEdge.Api.UnitTests
             Assert.Equal("Apples", item.Name);
             Assert.Equal(3, item.Quantity);
             Assert.Equal(DateTime.Parse("2020-01-01"), item.CreatedOn);
+        }
+
+        [Fact]
+        public void GetByNameNotFoundUnitTest()
+        {
+            var controller = new InventoryController(_loggerMock.Object);
+
+            var actionResult = controller.GetByName("dummy");
+
+            // Compare and cast. Expect an error.
+            Assert.IsType<NotFoundResult>(actionResult);
+        }
+
+        [Fact]
+        public void SuccessfulInsertPutUnitTest()
+        {
+            var controller = new InventoryController(_loggerMock.Object);
+
+            IActionResult actionResult = controller.Put(new InventoryItem()
+            {
+                Name = "Mangos", Quantity = 100, CreatedOn = DateTime.Now
+            });
+
+            // Compare and cast. Expect an error.
+            var acceptedResult = Assert.IsType<AcceptedResult>(actionResult);
+
+            var okResult = Assert.IsType<OkObjectResult>(controller.Get());
+
+            var list = Assert.IsAssignableFrom<ICollection<InventoryItem>>(
+                okResult.Value);
+
+            Assert.Equal(4, list.Count);
+            Assert.Contains(list, _ => _.Name.Equals("Oranges"));
+            Assert.Contains(list, _ => _.Name.Equals("Apples"));
+            Assert.Contains(list, _ => _.Name.Equals("Pomegranates"));
+            Assert.Contains(list, _ => _.Name.Equals("Mangos"));
+        }
+
+        [Fact]
+        public void SuccessfulUpdatePutUnitTest()
+        {
+            var controller = new InventoryController(_loggerMock.Object);
+
+            IActionResult actionResult = controller.Put(new InventoryItem()
+            {
+                Name = "Mangos",
+                Quantity = 200,
+                CreatedOn = DateTime.Now
+            });
+
+            // Compare and cast. Expect an error.
+            var acceptedResult = Assert.IsType<AcceptedResult>(actionResult);
+
+            var okResult = Assert.IsType<OkObjectResult>(controller.Get());
+
+            var list = Assert.IsAssignableFrom<ICollection<InventoryItem>>(
+                okResult.Value);
+
+            Assert.Contains(list, _ => _.Name.Equals("Mangos"));
+            var item = list.Single(_ => _.Name.Equals("Mangos"));
+            Assert.Equal(200, item.Quantity);
+        }
+
+        [Fact]
+        public void GetByMostPopularUnitTest()
+        {
+            var controller = new InventoryController(_loggerMock.Object);
+
+            var actionResult = controller.Get(null, QueryOptions.MostActivity);
+
+            // Compare and cast. Expect no errors.
+            var okResult = Assert.IsType<OkObjectResult>(actionResult);
+
+            var list = Assert.IsAssignableFrom<ICollection<InventoryItem>>(
+                okResult.Value);
+
+            Assert.Contains(list, _ => _.Name.Equals("Mangos"));
         }
     }
 }
